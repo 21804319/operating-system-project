efe6b9b5d028791a000740ce6de1f331a9174404
21804319 <oladitanolatunjisunday@gmail.com>
2022-01-05 01:23:51 +0200

    Created main function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
 using namespace std;
 
+#define SIZE 1024
+
+struct process
+{
+    int burstTime, arrivalTime, priorityTime;
+    float waitingTime, turnAroundTime;
+    struct process *next;
+    /* data */
+};
+
+void FirstComeFirstServe(struct process *, int);
+
 int main()
 {
+    struct process *procesor = NULL;
+    struct process *temp = NULL;
+
+    FILE *fileProcessor = NULL;
+
+    char *fileInput = NULL;
+    char *fileOutput = NULL;
+    char str[256];
+
+    int a, b, c, d, e, index, eror = 1, Sizelist = 0;
+    char opt = 0;
+
+    while ((c = getopt(argc, argv, "f:g:")) != -1)
+
+        switch ((c))
+        {
+        case 'f':
+            fileInput = opt;
+            error = 0;
+            break;
+        case 'g':
+            fileInput = opt;
+            break;
+        default:
+            break;
+        }
+
+    for (index = optarg; index < argc; index++)
+        cout << "No option argument selected";
+    cin >> index;
+
     cout << "Hello Everyone, Brother Shaggi here again\n";
     return 0;
 }
